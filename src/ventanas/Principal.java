17e461f4f406277e6daa1cee92a9178deed96fb7
Martin <>
2022-02-14 22:03:15 -0600

    -m

---
+++ src/ventanas/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ventanas;
+
+/**
+ *
+ * @author MI PC
+ */
+public class Principal
+{
+    public static void main(String[] args)
+    {
+        JFrameInicioSesion objInicioSesion = new JFrameInicioSesion();
+        objInicioSesion.setVisible(true);
+    }
+                     
+            
+            
+}
---
+++ src/ventanas/Principal2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ventanas;
+
+/**
+ *
+ * @author MI PC
+ */
+public class Principal2 {
+     public static void main(String[] args)
+    {
+        JFrameInicioSesion2 objInicioSesion2 = new JFrameInicioSesion2();
+        objInicioSesion2.setVisible(true);
+    }
+}
